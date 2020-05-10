c6aa98084598951182e36e95266db5d640e35adf
Simon Finnegan <sfinnegan@ff.local>
2020-05-10 21:34:28 +0100

    healthcheck fix

--- healthcheck.sh
+++ healthcheck.sh
@@ -12,7 +12,4 @@ do
 done
 
 # start the java command
-java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* \
-    -DHUB_HOST=$HUB_HOST \
-    -DBROWSER=$BROWSER \
-    org.testng.TestNG $MODULE
+java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* -DHUB_HOST=$HUB_HOST -DBROWSER=$BROWSER org.testng.TestNG $MODULE
